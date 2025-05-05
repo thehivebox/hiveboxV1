c161d4646f3904a8ccb6e853fa665a623de5a0b6
Dakota Halvorson <halvorsondakota@gmail.com>
2025-05-04 22:33:53 -0500

    Update Windows 11- Download Microsoft Teams (work or school) [teamsbootstrapper.exe].ps1

--- Windows 11- Download Microsoft Teams (work or school) [teamsbootstrapper.exe].ps1
+++ Windows 11- Download Microsoft Teams (work or school) [teamsbootstrapper.exe].ps1
@@ -2,13 +2,10 @@
 $url = "https://go.microsoft.com/fwlink/?linkid=2243204&clcid=0x409"
 
 # Define the path where the file will be saved
-$output = "$env:TEMP\downloaded_file.exe"
+$output = "C:\temp\teamsbootstrapper.exe"
 
 # Create a WebClient object
 $webClient = New-Object System.Net.WebClient
 
 # Download the file silently
 $webClient.DownloadFile($url, $output)
-
-# Output the path of the downloaded file
-Write-Output "File downloaded to: $output"
