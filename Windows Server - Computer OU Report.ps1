028823febc68d275e08e775bd675b0b488676e7d
Dakota Halvorson <halvorsondakota@gmail.com>
2025-05-07 21:05:55 -0500

    Update Windows Server - Computer OU Report.ps1

--- Windows Server - Computer OU Report.ps1
+++ Windows Server - Computer OU Report.ps1
@@ -1,10 +1,12 @@
+# This was created to extract all 'Name'(s) of Computers within the default, Computers OU within an Active Directory on a Windows Server. 
+
 # Load the AD Module
 Import-Module ActiveDirectory
 
 # Define the search base path for the "Computers" OU in the domain | replace "OU=Computers,DC=x,DC=x" with correct information.
 $searchBase = "OU=Computers,DC=x,DC=x"
 
-# Retrieve all computer objects in the specified OU | retrieve 'Name' and export the results to a CSV file.
+# Retrieve all computer objects in the specified OU | retrieve 'Name' and export the results to a CSV file. 
 Get-ADComputer -Filter * -SearchBase $searchBase |
     Select-Object Name |
     Export-Csv -Path "C:\temp\Computers.csv" -NoTypeInformation
