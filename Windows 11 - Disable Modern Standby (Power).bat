2a87a00b5287e3c88f454cf8fd1d94ecf1baf677
Dakota Halvorson <halvorsondakota@gmail.com>
2025-05-07 20:49:01 -0500

    Update Windows 11 - Disable Modern Standby (Power).bat

--- Windows 11 - Disable Modern Standby (Power).bat
+++ Windows 11 - Disable Modern Standby (Power).bat
@@ -1,3 +1,4 @@
+REM This registry key addition will add the old, Windows 10 "Power Management" Tab within the Wireless Network Adapter (Realek | Intel tested as working with WiFi 6) settings [Device Manager]. Restart the machine after adding the registry key for satisfactory result(s). 
 @echo off
 reg add HKLM\System\CurrentControlSet\Control\Power /v PlatformAoAcOverride /t REG_DWORD /d 0 /f >nul 2>&1
 exit /b
